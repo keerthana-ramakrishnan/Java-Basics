7a6f67bfa40ea0cba18293eefb1d08ee65c596f0
keerthana-ramakrishnan <126495696+keerthana-ramakrishnan@users.noreply.github.com>
2023-07-30 17:11:14 +0530

    Add files via upload

---
+++ Fibanocci.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Fibanocci
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter a no");
+        int a=sc.nextInt();
+        int f=0;
+        int g=1;
+        for (int i=1;i<=a;i++)
+        {
+            System.out.println(f);
+            f=f+g;
+            g=f-g;
+        }
+    }
+}
